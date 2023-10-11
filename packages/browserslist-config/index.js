998a3ffda7915692fa8755bb459d31303fd3334a
Ismo Vuorinen <ismo.vuorinen@vincit.fi>
2023-10-11 16:11:49 +0300

    feat(browserslist)!: update browser & node config

--- packages/browserslist-config/index.js
+++ packages/browserslist-config/index.js
@@ -1,13 +1,6 @@
 'use strict'
 
 module.exports = [
-  '>= 0.5%',
-  'Chrome >= 60',
-  'Firefox >= 60',
-  'Firefox ESR',
-  'iOS >= 12',
-  'last 2 major versions',
-  'not dead',
-  'not Explorer <= 11',
-  'Safari >= 12'
+  'defaults and fully supports es6-module',
+  'maintained node versions'
 ]
