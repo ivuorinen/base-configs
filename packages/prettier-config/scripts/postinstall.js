12eada58bc655ac55a9d6361e7b2bc10974abec4
Ismo Vuorinen <ismo.vuorinen@vincit.fi>
2023-10-17 15:11:21 +0300

    feat: skip config creation if one already found

--- packages/prettier-config/scripts/postinstall.js
+++ packages/prettier-config/scripts/postinstall.js
@@ -2,6 +2,44 @@
 
 const fs = require('fs')
 const path = require('path')
+const process = require('process')
+
+const findCosmicConfig = (moduleName) => {
+  const files = [
+    `.${moduleName}rc`,
+    `.${moduleName}rc.json`,
+    `.${moduleName}rc.yaml`,
+    `.${moduleName}rc.yml`,
+    `.${moduleName}rc.js`,
+    `.${moduleName}rc.ts`,
+    `.${moduleName}rc.mjs`,
+    `.${moduleName}rc.cjs`,
+    `.${moduleName}.jsonc`,
+    `.${moduleName}.yaml`,
+    `.${moduleName}.json`,
+    `${moduleName}rc`,
+    `${moduleName}rc.json`,
+    `${moduleName}rc.yaml`,
+    `${moduleName}rc.yml`,
+    `${moduleName}rc.js`,
+    `.config/${moduleName}rc.ts`,
+    `.config/${moduleName}rc.cjs`,
+    `${moduleName}.config.js`,
+    `${moduleName}.config.ts`,
+    `${moduleName}.config.mjs`,
+    `${moduleName}.config.cjs`
+  ]
+
+  const found = files.filter((file) => fs.existsSync(path.join(process.env.INIT_CWD, file)))
+
+  return found.length > 0
+}
+
+if (findCosmicConfig('prettier')) {
+  console.log('prettier-config: Found existing prettier config file, skipping creation.')
+  console.log('prettier-config: If you want to create a new config file, please remove the existing one.')
+  process.exit(0)
+}
 
 const filePath = path.join(process.env.INIT_CWD, '.prettierrc.json')
 
