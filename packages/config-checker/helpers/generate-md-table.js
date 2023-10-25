9d372493e844694781877cd4853d87198590a0ad
Ismo Vuorinen <ismo.vuorinen@vincit.fi>
2023-10-25 22:19:45 +0300

    feat(config-checker): new package: config-checker

---
+++ packages/config-checker/helpers/generate-md-table.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const configurationPaths = require('../lib/configuration-paths')
+
+const configItems = configurationPaths('[module name]')
+const longestLine = configItems.reduce((a, b) =>
+  a.length > b.length ? a : b
+).length
+
+const mdTable = configItems.map(file => {
+  const diff = longestLine - file.length
+  return `| ${file}${' '.repeat(diff)} |`
+})
+
+const header = 'Searched configuration files'
+console.log('| ' + header + ' '.repeat(longestLine - header.length) + ' |')
+console.log('| ' + '-'.repeat(longestLine) + ' |')
+
+mdTable.forEach(line => console.log(line))
---
+++ packages/config-checker/lib/configuration-paths.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const path = require('path')
+
+/**
+ * Returns an array of configuration paths.
+ * @param {string} moduleName - The name of the module to check for.
+ * @returns {string[]} - The paths to the configuration files.
+ */
+function configurationPaths(moduleName) {
+  const filesPlain = [
+    moduleName,
+    `${moduleName}rc`,
+    `${moduleName}rc.json`,
+    `${moduleName}rc.yaml`,
+    `${moduleName}rc.yml`,
+    `${moduleName}rc.js`,
+    `${moduleName}rc.ts`,
+    `${moduleName}rc.mjs`,
+    `${moduleName}rc.cjs`,
+    `${moduleName}.jsonc`,
+    `${moduleName}.yaml`,
+    `${moduleName}.json`,
+    `${moduleName}.config.js`,
+    `${moduleName}.config.ts`,
+    `${moduleName}.config.mjs`,
+    `${moduleName}.config.cjs`
+  ]
+
+  const filesDot = filesPlain.map(file => `.${file}`)
+  const bothFiles = filesPlain.concat(filesDot)
+  const filesInConfig = bothFiles.map(file => path.join('.config', file))
+
+  return bothFiles.concat(filesInConfig)
+}
+
+module.exports = configurationPaths
