52372dbe371bae56b44d79e2150b61200b071a1f
Ismo Vuorinen <11024+ivuorinen@users.noreply.github.com>
2024-05-05 15:27:18 +0300

    fix: add an initial value to "reduce()" call (#238)

--- packages/config-checker/helpers/generate-md-table.js
+++ packages/config-checker/helpers/generate-md-table.js
@@ -3,7 +3,7 @@
 const configurationPaths = require('../lib/configuration-paths')
 
 const configItems = configurationPaths('`[module name]`')
-const longestLine = configItems.reduce((a, b) => (a.length > b.length ? a : b)).length
+const longestLine = configItems.reduce((a, b) => (a.length > b.length ? a : b), 0).length
 
 const mdTable = configItems.map(file => {
   const diff = longestLine - file.length
